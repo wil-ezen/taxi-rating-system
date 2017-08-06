7016063a89294c5ea2e6a1fa787fe0599c6f7b9c
wil-ezen <estwil003@gmail.com>
2017-08-06 18:21:51 +0200

    - Created index.html for taxi - Changed index view to display form that accepts taxi number plate - Once entered, number plate's rating is displayed

--- taxi_rating_django/taxi/views.py
+++ taxi_rating_django/taxi/views.py
@@ -1,7 +1,28 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Taxi
 
+
 def index(request):
-    taxi_list = Taxi.objects.order_by('overall_rating')[:5]
-    output = ', '.join([str(t.overall_rating) for t in taxi_list])
-    return HttpResponse(output)
+    submitted_number_plate = request.POST['submitted_number_plate']
+    # This could return multiple number plates if the entry is not complete
+    submitted_number_plate_list = Taxi.objects.filter(number_plate__startswith=submitted_number_plate)
+    if not submitted_number_plate_list:
+        submitted_number_plate_rating = 0.0
+    else:
+        submitted_number_plate_rating = submitted_number_plate_list[0].overall_rating
+    template = loader.get_template('taxi/index.html')
+    # submitted_number_plate = 'Default string'
+    context = {
+        'submitted_number_plate': submitted_number_plate,
+        'submitted_number_plate_rating': submitted_number_plate_rating
+    }
+    return HttpResponse(template.render(context, request))
+
+
+# def details(request):
+#     return HttpResponse('HELLO FROM DETAILS!!!!')
+
+
+# def details(request, submitted_number_plate):
+#     return HttpResponse('Details for %s:' % submitted_number_plate)
